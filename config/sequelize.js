d6c34f2d5734e023737f1fdb0673696856381d65
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-24 03:29:05 -0300

    feat: add sequelize config

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-const configDatabse = require('./database');
+const configDatabse = require('../database/config/database');
 
 const sequelize = new Sequelize(configDatabse);
 
