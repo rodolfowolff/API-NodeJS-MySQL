38e50c907b3055a7d1a8a03032c8f85a6eabb809
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-27 22:05:29 -0300

    feat: get cliente by cpf

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -2,6 +2,24 @@ const { validationResult } = require('express-validator');
 const ClienteService = require('../services/clientes.service');
 
 class Controller {
+  getByCpf = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const cpf = req.params;
+      console.log(cpf, 'o cpf');
+      const result = await ClienteService.getByCpf(cpf);
+      console.log(result, 'result');
+      res.status(201).json(result);
+    } catch (error) {
+      res.status(404).json({ error: 'Clinte não encontrado' });
+    }
+  };
+
   register = async (req, res) => {
     const errors = validationResult(req);
 
@@ -11,14 +29,8 @@ class Controller {
 
     try {
       const { nome, cpf, email, cep, dataNascimento } = req.body;
-      const clienteRegister = await ClienteService
-        .clienteRegister({ nome, cpf, email, cep, dataNascimento });
-
-      if (clienteRegister) {
-        res.status(201).json({ message: 'Cliente cadastrado com sucesso!' });
-      } else {
-        res.status(400).json({ message: 'Cliente já cadastrado!' });
-      }
+      await ClienteService.clienteRegister({ nome, cpf, email, cep, dataNascimento });
+      res.status(201).json({ message: 'Cliente cadastrado com sucesso!' });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
--- src/services/clientes.service.js
+++ src/services/clientes.service.js
@@ -1,7 +1,24 @@
 const { Cliente } = require('../database/models');
 
 class Service {
+  getByCpf = async ({ cpf }) => {
+    const result = await Cliente.findOne({
+      where: { cpf },
+    });
+    if (!result) {
+      throw new Error('Cliente não encontrado');
+    }
+
+    return result;
+  };
+
   clienteRegister = async ({ nome, cpf, email, cep, dataNascimento }) => {
+    const findByCpf = await this.getByCpf({ cpf });
+
+    if (findByCpf) {
+      throw new Error('Cliente já cadastrado!');
+    }
+
     const [clienteRegister] = await Cliente.findOrCreate({
       where: { cpf },
       defaults: {
