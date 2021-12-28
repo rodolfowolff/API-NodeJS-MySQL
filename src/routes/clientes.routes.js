be12c6eba00a462cc9f3eb5b1ed4a02c77764af2
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-28 03:18:12 -0300

    feat: add route sendmail

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -3,9 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const ClienteController = require('../controllers/clientes.controller');
+const EmailController = require('../controllers/email.controller');
 const { validRegisterCliente } = require('../middlewares/validRegisterCliente');
 
 router.get('/:cpf', ClienteController.getByCpf);
+
+router.post('/enviaremail', EmailController.enviarEmail);
 router.post('/cadastro', validRegisterCliente, ClienteController.register);
 
 module.exports = router;
